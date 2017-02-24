eebd965484667bf179602ffc8da24c4693aa55fd
Pranav Dave <pdave1573@gmail.com>
2017-02-24 14:40:11 -0500

    store complete

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def index(request):
+    return render(request, 'template.html')
+
+def store(request):
+    count = Book.objects.all().count()
+    context = {
+        'count' : count,
+    }
+    return render(request, 'main.html', context)
